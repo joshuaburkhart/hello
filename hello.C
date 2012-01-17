5fd789dabd8539a9ff786413ec23d6502b8bb1e3
root <root@Mint12-VirtualBox.(none)>
2012-01-16 21:01:34 -0500

    simplified program.. should be what prof wants

--- hello.C
+++ hello.C
@@ -22,8 +22,7 @@ using std::string;
 
 #include "mpi.h"
 
-#include <sstream>
-using std::ostringstream;
+#include <string.h> //memset
 
 int main(int argc, char *argv[])
 {
@@ -48,34 +47,22 @@ int main(int argc, char *argv[])
       string msg = ((i < n) ? numbers[i] : "!");
       msg += '\0';
       msg.copy(buf,msg.length());
-      MPI_Send(buf,msg.length(),MPI_CHAR,i,0,MPI_COMM_WORLD);
+      MPI_Ssend(buf,msg.length(),MPI_CHAR,i,0,MPI_COMM_WORLD);
     }
-  ostringstream os1;
-  MPI_Barrier(MPI_COMM_WORLD); //wait for slaves to finish printing to stdout
+  memset(buf,0,10);
+  printf("I'm at a place called vertigo\n");
   for(int i=1; i < nprocs; i++){
       MPI_Status t;
       MPI_Recv(buf,MAX_MSG_SIZE,MPI_CHAR,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&t);
-      os1 << buf << "\n";
+      cout << buf << endl;  
     }
-  printf("I'm at a place called vertigo\n");
-  cout << os1.str() << endl;  
   }
   else {
     MPI_Status s;
     MPI_Recv(buf,MAX_MSG_SIZE,MPI_CHAR,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&s);
-    cout << buf << endl; //unsynchronous push to stdout
-    ostringstream oss;
-    oss << myid;
-    string msg="yeah from ";
-    msg+= oss.str();
-    msg+='\0';
+    cout << buf << endl; //asynchronous push to stdout
+    string msg="yeah";
     msg.copy(buf,msg.length());
-    MPI_Barrier(MPI_COMM_WORLD); /*
-				 MPI_Barrier here fixes cout racing issue.. 
-                                 I think what this is doing in the background is
-                                 forcing each thread to push it's private string
-                                 stream to stdout?
-				 */
     MPI_Send(buf,msg.length(),MPI_CHAR,0,0,MPI_COMM_WORLD);
   }
 
